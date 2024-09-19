2ee63ddf3ee29d6256171d1dbfd727c3ed4016d3
virginiaf23 <virginia24102003@gmail.com>
2024-09-19 11:04:13 +0700

    git

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -3,73 +3,83 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FOOTSAL</title>
-    <!-- Link ke app.css -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
-    <!-- Link ke Font Awesome CDN -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Payment Page</title>
+    <!-- Link to External CSS File -->
+    <link rel="stylesheet" href="{{ asset('css/payment.css') }}">
 </head>
 <body>
 
-    <div class="payment-page">
+    <div class="container">
         <!-- Gambar lapangan -->
-        <img src="{{ asset('assets/images/Group 23.png') }}" alt="Lapangan">
+        <img src="{{ asset('assets/images/Group 23.png') }}" class="lapangan" alt="lapangan">
 
-        <!-- Detail Pesanan -->
-        <div class="payment-details">
+        <!-- Info Section -->
+        <div class="info-section">
+            <div class="info-left">
+                <h2>Lapangan Futsal Romeo 5A</h2>
+                <div class="discount-area">
+                <img src="{{ asset('assets/images/discount.png') }}" class="diskon" alt="diskon">
+                    10% Discount area
+                </div>
+            </div>
+        </div>
+
+        <!-- Detail Section -->
+        <div class="detail-section">
             <!-- Detail Pesanan -->
-            <div class="section">
-                <h3>Detail Pesanan</h3>
+            <div>
+                <p class="detail-title">Detail Pesanan</p>
                 <div class="detail-item">
-                <img src="{{ asset('assets/images/time.png') }}" alt="Clock Icon">
-                    08.00 – 10.00 WIB
+                <img src="{{ asset('assets/images/time.png') }}" class="time" alt="time">
+                    08.00 - 10.00 WIB
                 </div>
                 <div class="detail-item">
-                <img src="{{ asset('assets/images/calendar.png') }}" alt="Calendar Icon">
+                <img src="{{ asset('assets/images/calendar.png') }}" class="calendar" alt="calendar">
                     Selasa, 20 Agustus 2024
                 </div>
                 <div class="detail-item">
-                <img src="{{ asset('assets/images/soccer-field.png') }}" alt="Size Icon">
+                <img src="{{ asset('assets/images/soccer-field.png') }}" class="soccer-field" alt="soccer-field">
                     18,8 m x 24,95 m
                 </div>
             </div>
 
             <!-- Data Pemesan -->
-            <div class="section">
-                <h3>Data Pemesan</h3>
+            <div>
+                <p class="detail-title">Data Pemesan</p>
                 <div class="detail-item">
-                <img src="{{ asset('assets/images/account.png') }}" alt="User Account">
+                <img src="{{ asset('assets/images/account.png') }}" class="account" alt="account">
                     Mohammad Farkhan
                 </div>
                 <div class="detail-item">
-                <img src="{{ asset('assets/images/phone.png') }}" alt="Phone Icon">
+                <img src="{{ asset('assets/images/phone.png') }}" class="phone" alt="phone">
                     +62 1234567890
                 </div>
             </div>
 
             <!-- Detail Harga -->
-            <div class="section price-details">
-                <h3>Detail Harga</h3>
-                <div class="price-item">
-                    <img src="{{ asset('assets/images/dollar.png') }}" alt="Price Icon">
-                    <span>Harga</span>
-                    <span>Rp. 500.000</span>
+            <div class="price-section">
+                <p class="detail-title">Detail Harga</p>
+                <div class="price-row">
+                    <img src="{{ asset('assets/images/dollar.png') }}" class="price-icon" alt="price">
+                    Harga: Rp. 500.000
                 </div>
-                <div class="price-item">
-                    <img src="{{ asset('assets/images/discount-line.png') }}" alt="diskon-line">
-                    <span>Potongan harga</span>
-                    <span>Rp. 50.000</span>
+                <div class="price-row">
+                    <img src="{{ asset('assets/images/discount-line.png') }}" class="discount-icon" alt="discount">
+                    Potongan harga: Rp. 50.000
                 </div>
-                <div class="price-item original-price">
-                    <span>Rp. 500.000</span>
+                <div class="total-price-section">
+                    <div class="total-text">Total Harga</div>
+                    <div class="price-detail">
+                        <div class="price-discount">Rp. 500.000</div>
+                        <div class="total-price">Rp. 450.000</div>
+                    </div>
                 </div>
-                <div class="total-price">Rp. 450.000</div>
             </div>
         </div>
 
         <!-- Metode Pembayaran -->
-        <h3>Metode Pembayaran</h3>
+        <p class="detail-title">Metode Pembayaran</p>
         <div class="payment-methods">
             <button class="selected">Bank BCA</button>
             <button>Bank BRI</button>
@@ -80,7 +90,7 @@
             <button>LINKAJA</button>
         </div>
 
-        <!-- Button Bayar -->
+        <!-- Pay Button -->
         <button class="pay-button">Bayar</button>
     </div>
 
